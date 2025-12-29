c173b78d0af72eaa67fb54345b16f9165012cc3b
Abdourahamane10 <amdroudjae@gmail.com>
2025-12-29 17:50:50 +0100

    Affichage dynamique du composant de droite en plus de SideNva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,24 @@ import { ConfirmeExpert } from './confirme-expert/confirme-expert';
 import { CreateConsultant } from './create-consultant/create-consultant';
 import { ConfirmeConsultant } from './confirme-consultant/confirme-consultant';
 import { Presences } from './presences/presences';
+import { SideNav } from './side-nav/side-nav';
+import { Planning } from './planning/planning';
+import { planningResolver } from './resolvers/planning/planning-resolver';
 
 export const routes: Routes = [
-    {path : '', component : Acceuil},
-    {path : 'login', component : Login},
-    {path : 'create-expert', component : CreateExpert},
-    {path : 'create-consultant', component : CreateConsultant},
-    {path : 'confirme-expert', component : ConfirmeExpert},
-    {path : 'confirme-consultant', component : ConfirmeConsultant},
-    {path : 'presences', component : Presences}
+  { path: '', component: Acceuil },
+  { path: 'login', component: Login },
+
+  {
+    path: '', // AppShell parent
+    component: SideNav,
+    children: [
+      { path: 'create-expert', component: CreateExpert },
+      { path: 'confirme-expert', component: ConfirmeExpert },
+      { path: 'create-consultant', component: CreateConsultant },
+      { path: 'confirme-consultant', component: ConfirmeConsultant },
+      { path: 'presences', component: Presences },
+      // { path: 'presences', component: Planning, resolve: { profiles: planningResolver } }
+    ],
+  },
 ];
