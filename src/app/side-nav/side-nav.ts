701d57a41c9041927c15bc66f2d1004c47eaf7ba
adelekekismath <adelekekismath9@gmail.com>
2026-01-29 14:57:50 +0100

    fix: make sidenav respons

--- src/app/side-nav/side-nav.ts
+++ src/app/side-nav/side-nav.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-side-nav',
   imports: [
     RouterLink,
     RouterOutlet,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule,
+    NgTemplateOutlet
   ],
   templateUrl: './side-nav.html',
   styleUrl: './side-nav.css',
