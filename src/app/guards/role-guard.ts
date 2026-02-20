3f76510fd103ab94a52992b0308f122d0e8f16ca
Semir MALICKY <arkaentech@gmail.com>
2026-02-20 11:55:35 +0100

    update of role into tab and store of the role through login

--- src/app/guards/role-guard.ts
+++ src/app/guards/role-guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { RoleService } from '../services/role/role-service';
+import { RoleService } from '../services/role/role.servcice';
 import { map, take } from 'rxjs';
 import { UserRole } from '../models/userRole';
 
@@ -8,14 +8,15 @@ export const roleGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const roleService = inject(RoleService);
 
-  return roleService.getRole$().pipe(
+  
+  return roleService.getRole().pipe(
     take(1),
     map(role => {
       console.log("role: ", role);
-      if (role === UserRole.CONSULTANT) {
+      if (role?.includes(UserRole.CONSULTANT)) {
         return true; //accès autorisé
       }
-      else if(role === UserRole.INSTRUCTEUR) {
+      else if(role?.includes(UserRole.INSTRUCTEUR) || role?.includes(UserRole.ENCADRANT)) {
         return router.parseUrl('/sidenav/planning'); //redirection
       }
       return false;
