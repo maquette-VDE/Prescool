43114d3232af41a59b6cea5615923a67cf9477fa
hmeyd <yyahmedsalm@example.com>
2026-03-13 16:19:40 +0100

    annonce details page

--- src/app/annonces/annonces.ts
+++ src/app/annonces/annonces.ts
@@ -1,15 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AnnonceService } from './annonce.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-annonce',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './annonces.html',
   styleUrl: './annonces.css'
 })
 export class Annonces {
   private annonceService = inject(AnnonceService);
   annonces = this.annonceService.getAnnonces();
+
+  projetSelectionne = signal<any>(null);
+
+selectionner(p: any) {
+  this.projetSelectionne.set(p);
+}
 }
