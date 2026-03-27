181a101c26b05dea638bfdbca7c592c796c3ada5
KIYE DENDI MOUBARAK <mkiyedendi@gmail.com>
2026-03-27 17:03:00 +0100

    Modification mot de passe Sécurié

--- src/app/services/planning/user.service.ts
+++ src/app/services/planning/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiConfigService } from '../api-config.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ import { Observable } from 'rxjs';
 export class UserService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = 'https://prez-cool-staging.appsolutions224.com/api/v1/users';
+  private readonly apiConfig = inject(ApiConfigService);
   getUserMe(): Observable<any> {
     return this.http.get(`${this.apiUrl}/me`);
   }
@@ -15,4 +18,13 @@ export class UserService {
   updateUserMe(userData: any): Observable<any> {
     return this.http.put(`${this.apiUrl}/me`, userData);
   }
+
+updateUserPassword(identifier: any, passwordData: any): Observable<any> {
+  // On construit l'URL exactement comme le Swagger le montre
+  // Assure-toi que identifier n'est pas 0 (on a vu 64 dans tes logs, c'est bon)
+  const url = this.apiConfig.buildUrl(`users/${identifier}/password`);
+
+  console.log("🚀 URL ENVOYÉE :", url);
+  return this.http.put(url, passwordData);
+}
 }
